6aae73effa3fc7481183fd03abbda30bbd029c63
ThoamsPinkert <thomas-pinkert@web.de>
2015-06-25 14:31:34 +0200

    5

---
+++ mylife/mylife/src/constants/ViewConstants.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace mylife.src.constants
+{
+	public static class ViewConstants
+	{
+		public static int TOP_PADDING = 20;
+		public static int SIDE_PADDING = 20;
+		public static int TITLE_HEIGHT = 100;
+
+		public static int TABLE_LEFT_PADDING = 30;
+		public static int TABLE_UNIT_PADDING = 10;
+		public static int TABLE_EDITABLE_TOP_BOTTOM_PADDING = 5;
+
+		public static int STANDART_CELL_HEIGHT = 50;
+		public static int NOTES_CELL_HEIGHT = 150;
+		public static int MARKER_CELL_HEIGHT = 100;
+
+		public static Color COLOR_TABLE_HEADER_STANDART = Color.FromRgb(130, 130, 130);
+		public static Color COLOR_TABLE_CELL_STANDART = Color.FromRgb(213, 213, 213);
+		public static Color COLOR_TABLE_CELL_EDIT_TEXT = Color.White;
+
+		public static int ROW_HEIGHT_BOTTOM_SAVE = 80;
+		public static int BUTTON_SAVE_HEIGHT = 50;
+		public static int BUTTON_SAVE_WIDTH = 100;
+	}
+}
+
